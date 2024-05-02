977f7a92f3f1e769b8a22c07678ee53d71fdb5c1
Rohit Mondal <rohit.luiji3@gmail.com>
2024-05-02 06:53:17 +0530

    fixes

--- surveyApp/static/analysis/export-pdf.js
+++ surveyApp/static/analysis/export-pdf.js
@@ -1,21 +1,21 @@
-// function exportPDF(){
-//     const invoice = this.document.getElementById("main");
-//             console.log(invoice);
-//             console.log(window);
-//             var opt = {
-//                 margin: 1,
-//                 filename: 'myfile.pdf',
-//                 image: { type: 'jpeg', quality: 0.98 },
-//                 html2canvas: { scale: 5 },
-//                 jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
-//             };
-//             html2pdf().from(invoice).set(opt).save();
-// }
+function exportPDF(){
+    const invoice = this.document.getElementById("master-div").cloneNode(true);
+            console.log(invoice);
+            console.log(window);
+            var opt = {
+                margin: 1,
+                filename: 'myfile.pdf',
+                image: { type: 'jpeg', quality: 0.98 },
+                html2canvas: { scale: 5 },
+                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+            };
+            html2pdf().from(invoice).set(opt).save();
+}
 
 function exportCSV() {
     const csvData = String(document.getElementById("csv").innerHTML);
 
-    const fileName =document.getElementById("td-title").innerHTML+ "data.csv";
+    const fileName =document.getElementById("td-title").innerHTML+ "  Report.csv";
     const blob = new Blob([csvData], { type: 'text/csv' });
     const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
